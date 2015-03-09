07ad880be2ed4248f3888196b44dad24b460f1e3
zerubeus <zerubeus@gmail.com>
2015-03-09 20:18:14 +0100

    adding some attributes to the Main Menu for Button and Skin

--- core/src/com/mygdx/eternity/screens/MainMenu.java
+++ core/src/com/mygdx/eternity/screens/MainMenu.java
@@ -1,48 +1,54 @@
 package com.mygdx.eternity.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class MainMenu implements Screen {
+	
+	private Stage stage;
+	private Skin skin;
+	private Table table;
+	private TextButton buttonPlay, ButtonExit;
+	private BitmapFont white, black;
+	private Label heading; 
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 
 	}
 
