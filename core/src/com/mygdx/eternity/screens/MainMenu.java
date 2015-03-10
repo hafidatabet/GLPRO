a6cde7ae13daa1ca915db1e9839ec058df65f340
zerubeus <zerubeus@gmail.com>
2015-03-10 01:26:06 +0100

    adding the menu skin and some other funcy stof to it but there is a crash accure because of the files name

--- core/src/com/mygdx/eternity/screens/MainMenu.java
+++ core/src/com/mygdx/eternity/screens/MainMenu.java
@@ -2,6 +2,7 @@ package com.mygdx.eternity.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class MainMenu implements Screen {
 	
@@ -16,20 +18,48 @@ public class MainMenu implements Screen {
 	private TextureAtlas atlas;
 	private Skin skin;
 	private Table table;
-	private TextButton buttonPlay, ButtonExit;
-	private BitmapFont white, black;
+	private TextButton buttonPlay, buttonExit;
+	private BitmapFont white;
 	private Label heading; 
 
 	@Override
-	public void show() {
-		white = new BitmapFont(Gdx.files.internal("font/white.fnt"));
-		black = new BitmapFont(Gdx.files.internal("font/black.fnt"));
-	}
-
-	@Override
 	public void render(float delta) {
-
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(delta);
+		stage.draw();
 	}
+	
+	@Override
+	public void show() {
+		stage = new Stage();
+		
+		atlas = new TextureAtlas("gui/button.pack");
+		skin = new Skin(atlas);
+		
+		
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		white = new BitmapFont(Gdx.files.internal("font/white.fnt"));
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		
+		textButtonStyle.up = skin.getDrawable("button_up");
+		textButtonStyle.down = skin.getDrawable("button_pressed");
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		textButtonStyle.font = white;
+
+		buttonExit = new TextButton("EXIT", textButtonStyle);
+		
+		buttonExit.pad(20);
+		
+		table.add(buttonExit);
+		table.debug();
+		stage.addActor(table);
+		
+	}
+
 
 	@Override
 	public void resize(int width, int height) {
